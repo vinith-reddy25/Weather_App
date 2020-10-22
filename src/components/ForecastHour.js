de5fcf1c7b0d3ccfc0f3f47048051ac3f4fb9e1a
vinith-reddy25 <64014016+vinith-reddy25@users.noreply.github.com>
2020-10-22 22:24:11 +0530

    Update ForecastHour.js

--- src/components/ForecastHour.js
+++ src/components/ForecastHour.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import SmallLabel from './SmallLabel';
+import {SmallLabel} from './MedLabel';
 import Text from './Text';
 import device from '../responsive/Device';
 
